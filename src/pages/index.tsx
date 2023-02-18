e853f2e3e28cee95e6ef2e9e1bbaf150b485c1d0
Sultan Yasin <sultan.yasin@utb.ecutbildning.se>
2023-02-18 01:01:55 +0100

    additional items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,41 @@
-import Head from "next/head";
+
 import Image from "next/image";
-import { Inter } from "@next/font/google";
+
 import styles from "@/styles/Home.module.css";
-import dumble from "../../assets/dumble.jpg";
+import Grid from "@mui/material/Grid";
 import shape from "../../assets/shape.jpg";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Grid className={styles.main}  container>
+        <Grid item>
+          <h3>
+            help for ideal <br /> body fitness
+          </h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,</p>
+          <button className="btn">Get started</button>
+        </Grid>
+        <Grid item>
+          
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+/*
+
+<Image
+            src={shape}
+            alt="Picture of the author"
+            width={600}
+            height={500}
+          />
+  <div className={styles.description}></div>
+<div className={styles.center}> </div>
+
+
       <Head>
         <title>Create Next App</title>
         <meta name="description" content="care about your health" />
@@ -23,37 +49,5 @@ export default function Home() {
           referrerpolicy="no-referrer"
         />
       </Head>
-      <main className={styles.main}>
-        <section className="home">
-          <div className="max-width">
-            <div className="home-content">
-              <h3>
-                help for ideal <br /> body fitness
-              </h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
-                numquam, sequi assumenda nam, vel nihil repudiandae omnis
-                eveniet est excepturi atque molestias at dolores fugit!
-              </p>
-              <button className="btn">Get started</button>
-            </div>
-
-            <Image
-              src={shape}
-              alt="Picture of the author"
-              width={600}
-              height={500}
-            />
-          </div>
-        </section>
-      </main>
-    </>
-  );
-}
-/*
-  <div className={styles.description}></div>
-<div className={styles.center}> </div>
-
-
        
 */
